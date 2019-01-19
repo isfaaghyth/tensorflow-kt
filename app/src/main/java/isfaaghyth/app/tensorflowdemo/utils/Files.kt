2c4e1dcd03267068a2464fa4923137ec743f8bf8
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-01-19 10:14:11 +0700

    load model file in utilities function

--- app/src/main/java/isfaaghyth/app/tensorflowdemo/utils/Files.kt
+++ app/src/main/java/isfaaghyth/app/tensorflowdemo/utils/Files.kt
@@ -1,10 +1,27 @@
 package isfaaghyth.app.tensorflowdemo.utils
 
+import android.content.res.AssetManager
+import java.io.FileInputStream
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+
 /**
  * Created by isfaaghyth on 19/01/19.
  * github: @isfaaghyth
  */
 object Files {
+
+    fun model(assetManager: AssetManager, path: String): MappedByteBuffer {
+        val file = assetManager.openFd(path)
+        val inputStream = FileInputStream(file.fileDescriptor)
+        val fileChannel = inputStream.channel
+
+        //offset
+        val start = file.startOffset
+        val length = file.declaredLength
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, start, length)
+    }
 
 
 
